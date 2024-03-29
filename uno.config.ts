f9068bcada5d8cfd5399cfc3e4def6969280e591
Maximilien Gilet <maximilien248@gmail.com>
2024-03-29 11:45:02 +0100

    feat: initialisation du projet

---
+++ uno.config.ts
@@ -0,0 +1,9 @@
+import { defineConfig } from "unocss";
+
+export default defineConfig({
+  shortcuts: {
+    // custom the default background
+    "bg-main": "bg-white text-black dark:(bg-[#1F2937] text-white)",
+  },
+  // ...
+});
